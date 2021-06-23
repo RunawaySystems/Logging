c851c7a84dac7be174bc758a60342aad485690a6
Nathan Lucci <nathan.p.lucci@pm.me>
2021-06-23 10:47:25 -0500

    GodotEditorLogger now configurable

--- Godot/GodotEditorLogger.cs
+++ Godot/GodotEditorLogger.cs
@@ -1,22 +1,47 @@
-﻿using Godot;
+﻿using System.Text;
+using Godot;
 
 namespace RunawaySystems.Logging {
+    /// <summary> Output target for Godot's editor console. (using GD.Print) </summary>
     public class GodotEditorLogger : ILogger {
 
-        public GodotEditorLogger() {
+        // parameters
+        bool includeWriteTime;
+        bool includeCaller;
+        bool includeVerbosity;
+
+        // state
+        private StringBuilder logWriter = new StringBuilder();
+
+        public GodotEditorLogger(bool includeWriteTime, bool includeCaller, bool includeVerbosity) {
+            this.includeWriteTime = includeWriteTime;
+            this.includeCaller = includeCaller;
+            this.includeVerbosity = includeVerbosity;
+
             Log.MessageLogged += Write;
         }
 
         public void Write(LogEntry entry) {
-            GD.Print($"[{entry.WriteTime}] {entry.Verbosity.ToString().Capitalize()}: {entry.Caller}: {entry.Message}");
+            if (includeWriteTime) 
+                logWriter.Append($"[{entry.WriteTime}] ");
+            if (includeCaller)
+                logWriter.Append($"{entry.Caller}: ");
+            if (includeVerbosity)
+                logWriter.Append($"{entry.Verbosity}: ");
+
+
+            logWriter.Append(entry.Message);
+            GD.Print(logWriter.ToString());
+            logWriter.Clear();
         }
 
         public override bool Equals(object obj) => obj is GodotEditorLogger;
     }
 
     public static partial class LoggerExtensions {
-        public static Logger WithGodotEditorLogging(this Logger logger) {
-            logger.Register(new GodotEditorLogger());
+        /// <inheritdoc cref="GodotEditorLogger"/>
+        public static Logger WithGodotEditorLogging(this Logger logger, bool includeWriteTime = false, bool includeCaller = true, bool includeVerbosity = true) {
+            Logger.Register(new GodotEditorLogger(includeWriteTime, includeVerbosity, includeCaller));
             return logger;
         }
     }
